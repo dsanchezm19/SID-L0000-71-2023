4e01f12dcb5d6460b93a9364ffec3aeed1564a66
Denisse Sanchez Miranda <denisse.sanchez@cfe.mx>
2025-08-28 12:13:48 -0600

    Se crea la páctica 4 en visual studio

---
+++ Practicas/Practica 4/2 Solution/ApiClientTest/OtrasPruebasYDocumentosTests.cs
@@ -0,0 +1,29 @@
+﻿using ApiClientLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClientTest
+{
+    public class OtrasPruebasYDocumentosTests
+    {
+        private readonly F1_ConfiguracionInicial _servicio;
+        public OtrasPruebasYDocumentosTests()
+        {
+            _servicio = new F1_ConfiguracionInicial();
+        }
+
+        [Fact(DisplayName = "Registrar Otras pruebas - Caso exitoso")]
+        public async Task RegistrarOtrasPruebas_Exitoso()
+        {
+            // Act
+            //var response = await _servicio.RegistrarOtrasPruebas();
+
+            //// Assert
+            //Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
