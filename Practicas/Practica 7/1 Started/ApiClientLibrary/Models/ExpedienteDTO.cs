64cb0c211d0e1a37d75e0b80f12682185c71fd0f
Denisse Sanchez Miranda <denisse.sanchez@cfe.mx>
2025-09-04 17:10:40 -0600

    Se actualiza started practica 7

--- Practicas/Practica 7/1 Started/ApiClientLibrary/Models/ExpedienteDTO.cs
+++ Practicas/Practica 7/1 Started/ApiClientLibrary/Models/ExpedienteDTO.cs
@@ -10,15 +10,8 @@ namespace ApiClientLibrary.Models
     {
         public string Id { get; set; }
         public string ClaveExpediente { get; set; }
-        public IList<MuestraDTO> MuestrasExpediente { get; set; }
         public int TamanioMuestra { get; set; }
-        //public IList<ResultadoPruebaDTO> ResultadosPruebas { get; set; }
-        //public OrdenFabricacionDTO OrdenFabricacion { get; set; }
-        public IList<string> AvisosPrueba { get; set; }
         public string EstatusPruebas { get; set; }
-        public string ResultadoExpediente { get; set; }
-        public DateTime InicioPruebas { get; set; }
-        public DateTime FinPruebas { get; set; }
-        public DateTime FechaRegistro { get; set; }
+        public List<MuestraDTO> MuestrasExpediente { get; set; }
     }
 }
