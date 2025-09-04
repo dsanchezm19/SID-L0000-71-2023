26a04ffa50a765dcfc6b5828be7d1e2f38034766
PEDRO ANTONIO FRIAS PULIDO <pedro.frias@cfe.mx>
2025-09-04 09:46:54 -0600

    Se agregan Practicas 3 6 y 8, e implementacion de PRactica 8 (inconclusa)

---
+++ Practicas/Practica 8/2 Solution/ApiClientLibrary/Models/ExpedienteSIDDTO.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ApiClientLibrary.Models
+{
+    public class ExpedienteSIDDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("claveExpediente")]
+        public string ClaveExpediente { get; set; }
+
+        [JsonPropertyName("muestrasExpediente")]
+        public List<MuestraExpedienteDTO> MuestrasExpediente { get; set; }
+
+        [JsonPropertyName("tamanioMuestra")]
+        public int TamanioMuestra { get; set; }
+
+        [JsonPropertyName("ordenFabricacion")]
+        public OrdenFabricacionDTO OrdenFabricacion { get; set; }
+
+        [JsonPropertyName("avisosPrueba")]
+        public List<object> AvisosPrueba { get; set; }
+
+        [JsonPropertyName("estatusPruebas")]
+        public string EstatusPruebas { get; set; }
+
+        [JsonPropertyName("resultadoExpediente")]
+        public string ResultadoExpediente { get; set; }
+
+        [JsonPropertyName("inicioPruebas")]
+        public DateTime InicioPruebas { get; set; }
+
+        [JsonPropertyName("finPruebas")]
+        public DateTime FinPruebas { get; set; }
+
+        [JsonPropertyName("fechaRegistro")]
+        public DateTime FechaRegistro { get; set; }
+    }
+
+    public class MuestraExpedienteDTO
+    {
+        [JsonPropertyName("identificador")]
+        public string Identificador { get; set; }
+
+        [JsonPropertyName("estatus")]
+        public string Estatus { get; set; }
+
+        [JsonPropertyName("resultadosPruebas")]
+        public List<ResultadoPruebaRequestDTO> ResultadosPruebas { get; set; }
+    }
+
+    public class ResultadoPruebaRequestDTO
+    {
+        [JsonPropertyName("prueba")]
+        public PruebaDTO Prueba { get; set; }
+
+        [JsonPropertyName("valorReferencia")]
+        public ValorReferenciaDTO ValorReferencia { get; set; }
+
+        [JsonPropertyName("fechaPrueba")]
+        public DateTime FechaPrueba { get; set; }
+
+        [JsonPropertyName("operadorPrueba")]
+        public string OperadorPrueba { get; set; }
+
+        [JsonPropertyName("instrumentoMedicion")]
+        public InstrumentoMedicionDTO InstrumentoMedicion { get; set; }
+
+        [JsonPropertyName("valorMedido")]
+        public decimal ValorMedido { get; set; }
+
+        [JsonPropertyName("resultado")]
+        public string Resultado { get; set; }
+
+        [JsonPropertyName("numeroIntento")]
+        public int NumeroIntento { get; set; }
+    }
+
+    public class PruebaDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("nombre")]
+        public string Nombre { get; set; }
+
+        [JsonPropertyName("estatus")]
+        public string Estatus { get; set; }
+
+        [JsonPropertyName("tipoPrueba")]
+        public string TipoPrueba { get; set; }
+
+        [JsonPropertyName("tipoResultado")]
+        public string TipoResultado { get; set; }
+
+        [JsonPropertyName("fechaRegistro")]
+        public DateTime FechaRegistro { get; set; }
+    }
+
+    public class ValorReferenciaDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("idProducto")]
+        public string IdProducto { get; set; }
+
+        [JsonPropertyName("idPrueba")]
+        public string IdPrueba { get; set; }
+
+        [JsonPropertyName("valor")]
+        public decimal Valor { get; set; }
+
+        [JsonPropertyName("valor2")]
+        public decimal Valor2 { get; set; }
+
+        [JsonPropertyName("unidad")]
+        public string Unidad { get; set; }
+
+        [JsonPropertyName("comparacion")]
+        public string Comparacion { get; set; }
+
+        [JsonPropertyName("fechaRegistro")]
+        public DateTime FechaRegistro { get; set; }
+    }
+
+    public class InstrumentoMedicionDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("nombre")]
+        public string Nombre { get; set; }
+
+        [JsonPropertyName("numeroSerie")]
+        public string NumeroSerie { get; set; }
+
+        [JsonPropertyName("fechaCalibracion")]
+        public DateTime FechaCalibracion { get; set; }
+
+        [JsonPropertyName("fechaVencimientoCalibracion")]
+        public DateTime FechaVencimientoCalibracion { get; set; }
+
+        [JsonPropertyName("urlArchivo")]
+        public string UrlArchivo { get; set; }
+
+        [JsonPropertyName("mD5")]
+        public string Md5 { get; set; }
+
+        [JsonPropertyName("estatus")]
+        public string Estatus { get; set; }
+
+        [JsonPropertyName("fechaRegistro")]
+        public DateTime FechaRegistro { get; set; }
+    }
+
+    public class OrdenFabricacionDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("claveOrdenFabricacion")]
+        public string ClaveOrdenFabricacion { get; set; }
+
+        [JsonPropertyName("loteFabricacion")]
+        public string LoteFabricacion { get; set; }
+
+        [JsonPropertyName("idProducto")]
+        public string IdProducto { get; set; }
+
+        [JsonPropertyName("detalleFabricacion")]
+        public List<DetalleFabricacionDTO> DetalleFabricacion { get; set; }
+    }
+
+    public class DetalleFabricacionDTO
+    {
+        [JsonPropertyName("contratoId")]
+        public string ContratoId { get; set; }
+
+        [JsonPropertyName("tipoContrato")]
+        public string TipoContrato { get; set; }
+
+        [JsonPropertyName("partidaContratoId")]
+        public string PartidaContratoId { get; set; }
+
+        [JsonPropertyName("descripcionPartida")]
+        public string DescripcionPartida { get; set; }
+
+        [JsonPropertyName("unidad")]
+        public string Unidad { get; set; }
+
+        [JsonPropertyName("cantidadOriginalContrato")]
+        public int CantidadOriginalContrato { get; set; }
+
+        [JsonPropertyName("cantidadAFabricar")]
+        public int CantidadAFabricar { get; set; }
+    }
+}
---
+++ Practicas/Practica 8/2 Solution/ApiClientLibrary/Services/F3_Pruebas.cs
@@ -0,0 +1,138 @@
+﻿using ApiClientLibrary.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Markup;
+
+namespace ApiClientLibrary.Services
+{
+    public class F3_Pruebas
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly string _basePath = "F3_Pruebas/";
+        public F3_Pruebas()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            _configuration = builder.Build();
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri($"{_configuration["ApiSettings:BaseUrl"]}{_basePath}")
+            };
+
+            var token = _configuration["ApiSettings:Token"];
+            if (!string.IsNullOrEmpty(token))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+
+        /// <summary>
+        /// Permite cambiar manualmente el token de autorización (ej. para pruebas).
+        /// </summary>
+        public void SetToken(string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        private async Task<HttpResponseMessage> PostAsync(EstadoSIDDTO estado)
+        {
+            var json = JsonSerializer.Serialize(estado);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("EstadoSID", content);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> RegistrarEstadoSID()
+        {
+            var estado = new EstadoSIDDTO { Estado = "EN_PRUEBAS" };
+            HttpResponseMessage response = await PostAsync(estado);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> RegistrarEstadoSID_DatosInvalidos()
+        {
+            var estado = new EstadoSIDDTO { Estado = "" };
+            HttpResponseMessage response = await PostAsync(estado);
+            return response;
+        }
+
+        // --------------------------------------------------------------------------
+        // Métodos para pruebas del SID
+        // --------------------------------------------------------------------------
+
+        /// <summary>
+        ///  Obtener el expediente por su clave en el SID.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ExpedienteSIDDTO> ObtenerExpedientePorClaveSID()
+        {
+            var claveExpediente = "EXP-01";
+            var response = await _httpClient.GetAsync($"ConsultaExpediente/{claveExpediente}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var expedientes = JsonSerializer.Deserialize<List<ExpedienteSIDDTO>>(jsonResponse,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                // Como solo esperas uno, regresamos el primero
+                return expedientes?.FirstOrDefault();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Agregar un resultado de prueba a una muestra en el SID.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<bool> AgregarResultadoPruebaAsync() {
+            var claveExpediente = "EXP-02";
+            var idMuestra = "M01";
+
+            // Creamos el body con ejemplo de datos
+            var resultado = new ResultadoPruebaRequestSIDDTO
+            {
+                IdPrueba = "687a83d143657ba3e593df9f",
+                IdValorReferencia = "687a8ed79a031ead55f89972",
+                FechaPrueba = DateTime.Parse("2025-09-03T19:40:08.582Z"),
+                OperadorPrueba = "Operador01",
+                IdInstrumentoMedicion = "68a77eb9be14ddad3f158293",
+                ValorMedido = 105,
+                Resultado = "SATISFACTORIO",
+                NumeroIntento = 1
+            };
+
+            var jsonContent = JsonContent.Create(resultado);
+
+            var response = await _httpClient.PutAsync($"AgregaResultadoPrueba?expediente={claveExpediente}&muestra={idMuestra}",jsonContent);
+            // Retornamos true si fue exitoso, false si no
+            return response.IsSuccessStatusCode;
+
+        }
+
+        /// <summary>
+        /// Indicar que las pruebas de un expediente en el SID fueron satisfactorias.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> IndicarResultadoSatisfactorioPruebasSIDAsync() { 
+            var claveExpediente = "EXP-02";
+
+            var response = await _httpClient.PutAsync($"TerminarPruebasExpediente/{claveExpediente}");
+            // Retornamos true si fue exitoso, false si no
+            return response.IsSuccessStatusCode;
+        }
+
+    }
+}
